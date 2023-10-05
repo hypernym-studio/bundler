99def53c6dbda637b4db67228564747f2b13b6cf
Ivo Dolenc <ivodolenc@gmail.com>
2023-10-05 21:56:32 +0200

    refactor: add global externals

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,13 @@
 import type { Options } from './types/index.js'
 
+export const externals = [
+  /^node:/,
+  /^@types/,
+  /^@rollup/,
+  /^@hypernym/,
+  /^rollup/,
+]
+
 export function defineConfig(options: Options): Options {
   return options
 }
