3f07cf601904ba0b3632c61778b13df93f1e1736
Ivo Dolenc <ivodolenc@gmail.com>
2023-10-05 21:58:20 +0200

    refactor: update main bundler config

--- bundler.config.ts
+++ bundler.config.ts
@@ -1,5 +1,20 @@
 import { defineConfig } from './src/config.js'
+import { version } from './package.json'
 
 export default defineConfig({
-  entries: ['./src/index.ts', './src/utils/index.ts'],
+  entries: [
+    { input: './src/index.ts', output: './dist/index.mjs' },
+    { input: './src/types/index.ts', output: './dist/types/index.d.ts' },
+    {
+      input: './src/bin/index.ts',
+      output: './dist/bin/index.mjs',
+      banner: '#!/usr/bin/env node',
+      plugins: {
+        replace: {
+          preventAssignment: true,
+          __version__: version,
+        },
+      },
+    },
+  ],
 })
