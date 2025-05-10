6b4b14bb9d8606b260d36a43b17dde344c55f0b0
Ivo Dolenc <ivodolenc@gmail.com>
2025-05-10 13:45:46 +0200

    chore: update main bundler config

--- bundler.config.ts
+++ bundler.config.ts
@@ -4,14 +4,14 @@ import { version } from './package.json'
 export default defineConfig({
   entries: [
     { input: './src/index.ts' },
-    { dts: './src/types/index.ts' },
+    { dts: './src/types/index.ts', output: './dist/index.d.mts' },
     {
       input: './src/index.ts',
       output: './dist/index.cjs',
     },
     {
       dts: './src/types/index.ts',
-      output: './dist/types/index.d.cts',
+      output: './dist/index.d.cts',
     },
     {
       input: './src/bin/index.ts',
