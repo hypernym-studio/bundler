41da004c98b2be871bc8151c7d45e696d049d9dd
Ivo Dolenc <ivodolenc@gmail.com>
2025-08-22 14:48:32 +0200

    chore: update bundler main config

--- bundler.config.ts
+++ bundler.config.ts
@@ -1,25 +1,19 @@
 import { defineConfig } from '@/config'
+import { replacePlugin } from '@/plugins'
 import { version } from './package.json'
 
 export default defineConfig({
   entries: [
     { input: './src/index.ts' },
     { dts: './src/types/index.ts', output: './dist/index.d.mts' },
+    { input: './src/plugins/index.ts' },
     {
-      input: './src/index.ts',
-      output: './dist/index.cjs',
-    },
-    {
-      dts: './src/types/index.ts',
-      output: './dist/index.d.cts',
+      dts: './src/plugins/types/index.ts',
+      output: './dist/plugins/index.d.mts',
     },
     {
       input: './src/bin/index.ts',
-      transformers: {
-        replace: {
-          __version__: version,
-        },
-      },
+      plugins: [replacePlugin({ __version__: version })],
     },
   ],
 })
