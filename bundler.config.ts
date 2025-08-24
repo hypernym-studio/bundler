808bbaf2bafaf5a9f84edd1ca62c9bc04155437f
Ivo Dolenc <ivodolenc@gmail.com>
2025-08-24 19:24:11 +0200

    chore: update bundler main config

--- bundler.config.ts
+++ bundler.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from '@/config'
+import { defineConfig, externals } from '@/config'
 import { replacePlugin } from '@/plugins'
 import { version } from './package.json'
 
 export default defineConfig({
   entries: [
-    { input: './src/index.ts' },
+    {
+      input: './src/bin/build.ts',
+      output: './dist/build/index.mjs',
+      externals: [...externals, '@/plugins'],
+      paths: [{ find: '@/plugins', replacement: '../plugins/index.mjs' }],
+    },
+    {
+      input: './src/index.ts',
+      externals: [...externals, '@/bin/build'],
+      paths: [{ find: '@/bin/build', replacement: './build/index.mjs' }],
+    },
     { dts: './src/types/index.ts', output: './dist/index.d.mts' },
     { input: './src/plugins/index.ts' },
     {
@@ -13,6 +23,8 @@ export default defineConfig({
     },
     {
       input: './src/bin/index.ts',
+      externals: [...externals, './build'],
+      paths: [{ find: './build', replacement: '../build/index.mjs' }],
       plugins: [replacePlugin({ __version__: version })],
     },
   ],
