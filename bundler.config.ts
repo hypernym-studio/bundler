11bf915389af70a8eb4ecbda6f23c7368bcc3639
Ivo Dolenc <ivodolenc@gmail.com>
2025-09-14 12:30:54 +0200

    chore: update bundler main config

--- bundler.config.ts
+++ bundler.config.ts
@@ -6,25 +6,25 @@ export default defineConfig({
   entries: [
     {
       input: './src/bin/build.ts',
-      output: './dist/build/index.mjs',
+      output: './dist/build/index.js',
       externals: [...externals, '@/plugins'],
-      paths: [{ find: '@/plugins', replacement: '../plugins/index.mjs' }],
+      paths: [{ find: '@/plugins', replacement: '../plugins/index.js' }],
     },
     {
       input: './src/index.ts',
       externals: [...externals, '@/bin/build'],
-      paths: [{ find: '@/bin/build', replacement: './build/index.mjs' }],
+      paths: [{ find: '@/bin/build', replacement: './build/index.js' }],
     },
-    { dts: './src/types/index.ts', output: './dist/index.d.mts' },
+    { dts: './src/types/index.ts', output: './dist/index.d.ts' },
     { input: './src/plugins/index.ts' },
     {
       dts: './src/plugins/types/index.ts',
-      output: './dist/plugins/index.d.mts',
+      output: './dist/plugins/index.d.ts',
     },
     {
       input: './src/bin/index.ts',
       externals: [...externals, './build'],
-      paths: [{ find: './build', replacement: '../build/index.mjs' }],
+      paths: [{ find: './build', replacement: '../build/index.js' }],
       plugins: [replacePlugin({ __version__: version })],
     },
   ],
